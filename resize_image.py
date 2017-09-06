1dbf3c3762a6e435b43c1545436367719874d2ea
smile <smilence86@gamil.com>
2017-09-06 21:06:13 +0800

    add one scenes

--- resize_image.py
+++ resize_image.py
@@ -18,12 +18,13 @@ basewidth = 128
 for image in list:
     id_tag = image.find(".")
     name = image[0:id_tag]
-    print(name)
+    # print(name)
     ext = image[id_tag:]
     # print(ext)
 
     img = Image.open(src + image)
     if img.size[0] == 640:
+        print(name)
         wpercent = (basewidth / float(img.size[0]))
         hsize = int((float(img.size[1]) * float(wpercent)))
         out = img.resize((basewidth, hsize))
